fec5495ac7811b91526f57c21b525a8c59712830
Hazar Ute <hazarute@gmail.com>
2015-11-05 03:41:14 +0300

    ado.NET
    first connection sql

---
+++ WindowsFormsApplication_FirstConnection/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication_FirstConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnRefresh_Click(null, null);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Server=HAZAR-NOTEBOOK;Database=User_Role_Sample;User Id=sa;Password=123456789?";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("InsertUpdate_Role", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Name", txtRoleName.Text);
+                command.Parameters.AddWithValue("@IsActive", chkIsActive.Checked);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                btnRefresh_Click(null, null);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=HAZAR-NOTEBOOK;Database=User_Role_Sample;User Id=sa;Password=123456789?";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                DataTable dt = new DataTable();
+                SqlCommand command = new SqlCommand("Select_Role", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(command);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                da.Fill(dt);
+                connection.Close();
+
+                dgvRole.DataSource = dt;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=HAZAR-NOTEBOOK;Database=User_Role_Sample;User Id=sa;Password=123456789?";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                
+                SqlCommand command = new SqlCommand("Delete_Role", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@RoleID", asd);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                btnRefresh_Click(null, null);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            
+            string connectionString = "Server=HAZAR-NOTEBOOK;Database=User_Role_Sample;User Id=sa;Password=123456789?";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("InsertUpdate_Role", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@RoleID", asd);
+                command.Parameters.AddWithValue("@Name", txtRoleName.Text);
+                command.Parameters.AddWithValue("@IsActive", chkIsActive.Checked);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                btnRefresh_Click(null, null);
+            }
+        }
+
+        int asd;
+
+        private void dgvRole_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dgvRole.SelectedRows)
+            {
+                asd = Convert.ToInt32(item.Cells[0].Value.ToString());
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication_FirstConnection/WindowsFormsApplication_FirstConnection/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApplication_FirstConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRole = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRoleName = new System.Windows.Forms.TextBox();
+            this.chkIsActive = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRole
+            // 
+            this.dgvRole.AllowUserToAddRows = false;
+            this.dgvRole.AllowUserToDeleteRows = false;
+            this.dgvRole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRole.Location = new System.Drawing.Point(12, 82);
+            this.dgvRole.MultiSelect = false;
+            this.dgvRole.Name = "dgvRole";
+            this.dgvRole.ReadOnly = true;
+            this.dgvRole.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRole.Size = new System.Drawing.Size(562, 354);
+            this.dgvRole.TabIndex = 0;
+            this.dgvRole.SelectionChanged += new System.EventHandler(this.dgvRole_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Role Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Is Active :";
+            // 
+            // txtRoleName
+            // 
+            this.txtRoleName.Location = new System.Drawing.Point(152, 6);
+            this.txtRoleName.Name = "txtRoleName";
+            this.txtRoleName.Size = new System.Drawing.Size(169, 32);
+            this.txtRoleName.TabIndex = 3;
+            // 
+            // chkIsActive
+            // 
+            this.chkIsActive.AutoSize = true;
+            this.chkIsActive.Checked = true;
+            this.chkIsActive.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkIsActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkIsActive.Location = new System.Drawing.Point(154, 55);
+            this.chkIsActive.Name = "chkIsActive";
+            this.chkIsActive.Size = new System.Drawing.Size(15, 14);
+            this.chkIsActive.TabIndex = 4;
+            this.chkIsActive.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(327, 6);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(74, 32);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(407, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(74, 32);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(487, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(87, 32);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(327, 44);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(98, 32);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 449);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.chkIsActive);
+            this.Controls.Add(this.txtRoleName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvRole);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRole)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRole;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRoleName;
+        private System.Windows.Forms.CheckBox chkIsActive;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
